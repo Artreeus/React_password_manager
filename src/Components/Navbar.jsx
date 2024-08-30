cc03915febe84dfb4c0fd203e45d83c87dad4d3f
Artreeus <142108262+Artreeus@users.noreply.github.com>
2024-08-31 02:00:08 +0600

    name changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="navbar flex justify-center text-white  ">
+    <div className="navbar flex justify-center text-white my-7  ">
       <div className="relative p-1 bg-gradient-to-r from-blue-500 to-pink-500 rounded-xl transition-all duration-400 ease-in-out ">
         <button className="text-3xl py-2 px-3 rounded-lg border-none  text-white cursor-pointer shadow-md">
           Password Manager
