a7fcc0a77668d988d85a4b853b773178c646bac2
Artreeus <142108262+Artreeus@users.noreply.github.com>
2024-08-31 18:23:15 +0600

    icon added

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Manager = () => {
+  // State to manage password visibility
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Function to toggle password visibility
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
       {/* content */}
-
       <div className="mx-auto mycontainer px-4 sm:px-8 lg:px-16">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center">
           <span className="text-green-700">&lt; </span>
@@ -19,7 +26,6 @@ const Manager = () => {
         </p>
 
         {/* input fields */}
-
         <form className="flex flex-col p-4 text-black gap-6 sm:gap-8 items-center">
           <input
             className="rounded-full border border-green-500 w-full p-4 py-2 text-sm sm:text-base"
@@ -36,13 +42,23 @@ const Manager = () => {
               placeholder="Enter Website Username"
               aria-label="Enter website username"
             />
-            <input
-              className="rounded-full border border-green-500 w-full p-4 py-2 text-sm sm:text-base"
-              type="password"
-              id="website-password"
-              placeholder="Enter Website Password"
-              aria-label="Enter website password"
-            />
+            <div className=" w-full flex items-center">
+              <input
+                className="rounded-full border border-green-500 w-full p-4 py-2 text-sm sm:text-base "
+                type={showPassword ? "text" : "password"}
+                id="website-password"
+                placeholder="Enter Website Password"
+                aria-label="Enter website password"
+              />
+              <lord-icon
+                src="https://cdn.lordicon.com/vfczflna.json"
+                trigger="hover"
+                colors="primary:#000,secondary:#0000"
+                style={{ width: '30px', height: '30px', margin:'-40px' }}
+                className=" cursor-pointer "
+                onClick={togglePasswordVisibility}
+              ></lord-icon>
+            </div>
           </div>
 
           {/* password btn */}
